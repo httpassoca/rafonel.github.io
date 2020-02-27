3f5b3e8028f3000bf06bc0cda01f61e57eec1f4b
Rafael Freitas <trabsom.rafael@gmail.com>
2020-02-27 10:18:00 -0300

    god plz help me

---
+++ precache-manifest.d6227089bd1eb1e57704cf2e59bfed58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e1901552263a72e7e6c78f9ec3df020",
+    "url": "/rafonel.github.io/index.html"
+  },
+  {
+    "revision": "ab00ad074a6a84ba7bc7",
+    "url": "/rafonel.github.io/static/css/main.0a625050.chunk.css"
+  },
+  {
+    "revision": "9b1c28fc096630cc72c9",
+    "url": "/rafonel.github.io/static/js/2.1178d0bb.chunk.js"
+  },
+  {
+    "revision": "6cfe7e9ff4ee3ea63d876714b47c578d",
+    "url": "/rafonel.github.io/static/js/2.1178d0bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab00ad074a6a84ba7bc7",
+    "url": "/rafonel.github.io/static/js/main.6b05c339.chunk.js"
+  },
+  {
+    "revision": "ea5473a1b5f9d5d1d47a",
+    "url": "/rafonel.github.io/static/js/runtime-main.b6c37b2f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/rafonel.github.io/precache-manifest.d6227089bd1eb1e57704cf2e59bfed58.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rafonel.github.io/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
