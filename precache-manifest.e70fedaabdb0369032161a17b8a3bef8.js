be58cd8fefcfb618f81a4c8a9461e1c912b9f382
Rafael Freitas <trabsom.rafael@gmail.com>
2020-02-27 10:45:25 -0300

    Updates

---
+++ precache-manifest.e70fedaabdb0369032161a17b8a3bef8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c6ffd55e95ea9f46c88eadb841c5b86",
+    "url": "/rafonel.github.io/index.html"
+  },
+  {
+    "revision": "dd829a0453b725c8dc3c",
+    "url": "/rafonel.github.io/static/css/main.b3c77254.chunk.css"
+  },
+  {
+    "revision": "068bc932bb1dbee36d79",
+    "url": "/rafonel.github.io/static/js/2.ac0abe19.chunk.js"
+  },
+  {
+    "revision": "bcdef9c0b1f30fbfbe08a32a4b06e5a8",
+    "url": "/rafonel.github.io/static/js/2.ac0abe19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd829a0453b725c8dc3c",
+    "url": "/rafonel.github.io/static/js/main.8dbdd3af.chunk.js"
+  },
+  {
+    "revision": "ea5473a1b5f9d5d1d47a",
+    "url": "/rafonel.github.io/static/js/runtime-main.b6c37b2f.js"
+  }
+]);
