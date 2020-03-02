8da07f32357c48be3e048394b006fdf3ea460836
Rafael Freitas <trabsom.rafael@gmail.com>
2020-03-02 19:23:29 -0300

    Fixing responsivity and added a Switch between pages

---
+++ precache-manifest.03366eda1440b8a1a8a035c7fd372db9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db72d69c570fc53b32277769adf3bd80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7f8ad797db5ad2b4d2c",
+    "url": "/static/css/main.ba35e210.chunk.css"
+  },
+  {
+    "revision": "c814e1321ea3d2deb198",
+    "url": "/static/js/2.1f18a304.chunk.js"
+  },
+  {
+    "revision": "8646eaaf0ceb7dd4299a17ba5537b98a",
+    "url": "/static/js/2.1f18a304.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7f8ad797db5ad2b4d2c",
+    "url": "/static/js/main.0783f641.chunk.js"
+  },
+  {
+    "revision": "6c3c189d332e651f6e1a",
+    "url": "/static/js/runtime-main.8c66b6fe.js"
+  },
+  {
+    "revision": "acc1816bbc7fe5a097918a98c3345fc9",
+    "url": "/static/media/Boston-Bold.acc1816b.otf"
+  },
+  {
+    "revision": "cb6321a84e8645ca68cb4000bda0e864",
+    "url": "/static/media/Boston-BoldIt.cb6321a8.otf"
+  },
+  {
+    "revision": "d8512eef209e2e91427a9171ce84f037",
+    "url": "/static/media/Boston-Light.d8512eef.otf"
+  },
+  {
+    "revision": "1c032baee4c40efe4ad94719d2f377f4",
+    "url": "/static/media/Boston-LightIt.1c032bae.otf"
+  },
+  {
+    "revision": "fdb83b251895a26eb58c7dd29a11c59b",
+    "url": "/static/media/Boston-Regular.fdb83b25.otf"
+  }
+]);
