1e09b24fb5257ca07217ed3139d0207da93861af
Rafael Freitas <trabsom.rafael@gmail.com>
2020-06-11 03:04:45 -0300

    m

---
+++ build/precache-manifest.2258bf1d5ed0716546380c5d02ec03a7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0087da6d528fd834de7c9d1b9391f89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ed4b8f88fbf465ad94e",
+    "url": "/static/css/main.00a118ab.chunk.css"
+  },
+  {
+    "revision": "21fe78d6730a726950e1",
+    "url": "/static/js/2.fc00dbe1.chunk.js"
+  },
+  {
+    "revision": "8646eaaf0ceb7dd4299a17ba5537b98a",
+    "url": "/static/js/2.fc00dbe1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ed4b8f88fbf465ad94e",
+    "url": "/static/js/main.8d670e8a.chunk.js"
+  },
+  {
+    "revision": "6c3c189d332e651f6e1a",
+    "url": "/static/js/runtime-main.8c66b6fe.js"
+  },
+  {
+    "revision": "acc1816bbc7fe5a097918a98c3345fc9",
+    "url": "/static/media/Boston-Bold.acc1816b.otf"
+  },
+  {
+    "revision": "cb6321a84e8645ca68cb4000bda0e864",
+    "url": "/static/media/Boston-BoldIt.cb6321a8.otf"
+  },
+  {
+    "revision": "d8512eef209e2e91427a9171ce84f037",
+    "url": "/static/media/Boston-Light.d8512eef.otf"
+  },
+  {
+    "revision": "1c032baee4c40efe4ad94719d2f377f4",
+    "url": "/static/media/Boston-LightIt.1c032bae.otf"
+  },
+  {
+    "revision": "fdb83b251895a26eb58c7dd29a11c59b",
+    "url": "/static/media/Boston-Regular.fdb83b25.otf"
+  }
+]);
