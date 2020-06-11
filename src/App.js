94baa5953ec6ca809c6b9743b73c25bec89ad225
Rafael Freitas <trabsom.rafael@gmail.com>
2020-06-11 02:47:06 -0300

    add language switch | remove blog | add projects page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import TrianglesCanvas from './components/AnimatedWallpaper'
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import { AnimatedSwitch } from 'react-router-transition';
@@ -6,15 +7,15 @@ import Switch from 'react-switch'
 import Transition from './utils/switchTransitionConfig';
 
 import "./App.sass";
-
+import BrazilIcon from "./assets/brazil.png"
 import Perfil from "./views/PerfilPage";
-import Blog from "./views/BlogPage";
+import Projects from "./views/ProjectsPage";
 import Experience from './views/ExperiencePage'
-import { useState } from "react";
 
 
 export default function App() {
   const [animateBackground, setAnimateBackground] = useState(true)
+  const [language, setLanguage] = useState(true)
   return (
     <>
       <BrowserRouter>
@@ -33,6 +34,20 @@ export default function App() {
             />
             <i className="gg-shape-triangle"></i>
           </div>
+          <div className="switch-field">
+            <Switch
+              checked={language}
+              uncheckedIcon={false}
+              checkedIcon={false}
+              onChange={() => {setLanguage(!language);}}
+              onColor="#00AA00" onHandleColor="#00FF00"
+              activeBoxShadow="0px 0px 0px 0px"
+              boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+              handleDiameter={20} height={15} width={30}
+              className="switchHome"
+            />
+            <img src={BrazilIcon} alt="pt br" class="iconBrazil"/>
+          </div>
         </div>
         <section id="section" className="section">
           <TrianglesCanvas active={animateBackground}/>
@@ -44,8 +59,8 @@ export default function App() {
             <Route exact path="/">
               <Perfil />
             </Route>
-            <Route exact path="/blog">
-              <Blog/>
+            <Route exact path="/projects">
+              <Projects/>
             </Route>
             <Route exact path="/exp">
               <Experience/>
@@ -57,7 +72,7 @@ export default function App() {
                 <Link to="/">Sobre mim</Link>
               </li>
               <li>
-                <Link to="/blog">Meu Blog</Link>
+                <Link to="/projects">Projetos</Link>
               </li>
               <li>
                 <Link to="/exp">Experiências</Link>
--- src/views/ProjectsPage.jsx
+++ src/views/ProjectsPage.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function BlogPage() {
   return (
       <div className="blog">
-        <p>My blog. Wait for more.</p>
+        <p>My projects. Wait for more.</p>
       </div>
   );
 }
