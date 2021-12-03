83ac8a8751eb3fd4b076f6db4d360fbc5f9d012a
rafonel <rfreitas@soujmv.com>
2021-12-03 13:08:34 -0300

    change links

--- src/components/Perfil/AboutImage.jsx
+++ src/components/Perfil/AboutImage.jsx
@@ -7,16 +7,16 @@ function AboutImg(){
                 <img src="https://avatars1.githubusercontent.com/u/49541181?s=400&u=1d3e9615ed572998f591aa6ad664879e259a43bc&v=4" alt="" className="pic" />
                 <div className="icons-perfil hide-on-900">
                     <Link href="https://www.behance.net/rafonel" icon="fab fa-behance" color="#0057ff"/>
-                    <Link href="https://github.com/Rafonel" icon="fab fa-github" color="#55277e"/>
-                    <Link href="https://www.linkedin.com/in/rafonel2511/" icon="fab fa-linkedin" color="#0077b5"/>
-                    <Link href="https://instagram.com/ra_fon_el/" icon="fab fa-instagram" color="#DD2A7B"/>
+                    <Link href="https://github.com/httpassoca" icon="fab fa-github" color="#55277e"/>
+                    <Link href="https://www.linkedin.com/in/passoca/" icon="fab fa-linkedin" color="#0077b5"/>
+                    <Link href="https://instagram.com/rafael.passoca/" icon="fab fa-instagram" color="#DD2A7B"/>
                 </div>
             </div>
             <div className="icons-perfil show-on-900">
                 <Link href="https://www.behance.net/rafonel" icon="fab fa-behance" color="#4d89ff" phone="true"/>
-                <Link href="https://github.com/Rafonel" icon="fab fa-github" color="#7c39b8" phone="true"/>
-                <Link href="https://www.linkedin.com/in/rafonel2511/" icon="fab fa-linkedin" color="#02a9ff" phone="true"/>
-                <Link href="https://instagram.com/ra_fon_el/" icon="fab fa-instagram" color="#e86ca4" phone="true"/>
+                <Link href="https://github.com/httpassoca" icon="fab fa-github" color="#7c39b8" phone="true"/>
+                <Link href="https://www.linkedin.com/in/passoca/" icon="fab fa-linkedin" color="#02a9ff" phone="true"/>
+                <Link href="https://instagram.com/rafael.passoca/" icon="fab fa-instagram" color="#e86ca4" phone="true"/>
             </div>
         </div>
     );
